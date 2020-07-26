19fc45706d284eaccbed8f82f53dc5b53a9e6325
Alexandre Dumont aka Adlx <adumont@users.noreply.github.com>
2020-07-26 14:43:58 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,7 +3,29 @@ input.onButtonPressed(Button.A, function () {
 })
 radio.onReceivedValue(function (name, value) {
     serial.writeValue(name, value)
+    if (name == "x") {
+        x = value
+    } else if (name == "y") {
+        y = value
+    } else if (name == "Dial") {
+        Dial = Math.map(value, 5, 950, 0, 255)
+    } else if (name == "Red") {
+        if (value == 1) {
+            maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, Dial)
+        } else {
+            maqueen.motorStop(maqueen.Motors.All)
+        }
+    } else if (false) {
+    	
+    } else if (false) {
+    	
+    } else {
+    	
+    }
 })
+let Dial = 0
+let y = 0
+let x = 0
 radio.setGroup(1)
 basic.showIcon(IconNames.Happy)
 basic.forever(function () {

86c6e0068d5ecd28b6a2556c51265b4a63b5576c
Alexandre Dumont aka Adlx <adumont@users.noreply.github.com>
2020-07-26 17:14:44 +0200

    Calibrated values for x/y

--- main.ts
+++ main.ts
@@ -1,12 +1,33 @@
+function Left (speed: number) {
+    if (speed > 0) {
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, speed)
+    } else if (speed < 0) {
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 0 - speed)
+    } else {
+        maqueen.motorStop(maqueen.Motors.M1)
+    }
+}
 input.onButtonPressed(Button.A, function () {
+    serial.writeLine("--")
     serial.writeValue("A", 1)
+    serial.writeValue("y", y)
+    serial.writeValue("x", x)
+    serial.writeValue("sLeft", Math.max(-255, Math.min((y + x) / 2, 255)))
+    serial.writeValue("sRight", Math.max(-255, Math.min((y - x) / 2, 255)))
 })
 radio.onReceivedValue(function (name, value) {
-    serial.writeValue(name, value)
     if (name == "x") {
-        x = value - 512
+        if (value >= 582) {
+            x = Math.map(value, 582, 1023, 0, 512)
+        } else {
+            x = Math.map(value, 0, 582, -512, 0)
+        }
     } else if (name == "y") {
-        y = value - 512
+        if (value >= 568) {
+            y = Math.map(value, 568, 1023, 0, 512)
+        } else {
+            y = Math.map(value, 0, 568, -512, 0)
+        }
     } else if (name == "Dial") {
         Dial = Math.map(value, 5, 800, 0, 255)
     } else if (name == "Red") {
@@ -23,12 +44,24 @@ radio.onReceivedValue(function (name, value) {
     	
     }
 })
+function Right (speed: number) {
+    if (speed > 0) {
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, speed)
+    } else if (speed < 0) {
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 0 - speed)
+    } else {
+        maqueen.motorStop(maqueen.Motors.M2)
+    }
+}
 let Dial = 0
-let y = 0
 let x = 0
+let y = 0
+y = 0
 radio.setGroup(1)
 basic.showIcon(IconNames.Happy)
+serial.writeLine("Started")
+serial.setBaudRate(BaudRate.BaudRate115200)
 basic.forever(function () {
-    maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, (y + x) / 4)
-    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, (y - x) / 4)
+    Left(Math.max(-255, Math.min((y + x) / 2, 255)))
+    Right(Math.max(-255, Math.min((y - x) / 2, 255)))
 })
